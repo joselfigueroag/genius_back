a7be0af7ff3692e40573b9a4529e86c17d6e4cb5
Jose Figueroa <joselfigueroag@gmail.com>
2024-01-13 20:14:23 -0400

    added model for comment storage

--- huggingface/models.py
+++ huggingface/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+  class EmotionLabel(models.TextChoices):
+    JOY = "joy"
+    FEAR = "fear"
+    SADNESS = "sadness"
+    SURPRISE = "surprise"
+    DISGUST = "disgust"
+    ANGER = "anger"
+    OTHERS = "others"
+    ERROR = "error"
+  
+  class SentimentLabel(models.TextChoices):
+    POS = "POS"
+    NEG = "NEG"
+    NEU = "NEU"
+    ERROR = "ERROR"
+
+  text = models.TextField(verbose_name="texto")
+  emotion_label = models.CharField(max_length=10, verbose_name="etiqueta de emocion", choices=EmotionLabel.choices)
+  sentiment_label = models.CharField(max_length=5, verbose_name="etiqueta sentimiento", choices=SentimentLabel.choices)
--- huggingface/process_data.py
+++ huggingface/process_data.py
@@ -1,5 +1,7 @@
 import requests
 
+from .models import Comment
+
 EMOTION_ENDPOINT = "https://api-inference.huggingface.co/models/finiteautomata/beto-emotion-analysis"
 SENTIMENT_ENDPOINT = "https://api-inference.huggingface.co/models/finiteautomata/beto-sentiment-analysis"
 
@@ -12,21 +14,45 @@ class ProcessData:
     )
 
   def emotion_model(self, data):
+    comment, created = Comment.objects.get_or_create(
+      text=data, defaults={"emotion_label": "error"}
+    )
+
+    if comment.emotion_label and comment.emotion_label != Comment.EmotionLabel.ERROR:
+      print("Procesado por DB")
+      return comment.emotion_label
+    
+    print("Procesado por API")
     response = self.httpClient.request("POST", EMOTION_ENDPOINT, json=data)
     if response.status_code == 200:
       label = response.json()[0][0]["label"]
     else:
       label = "error"
       print(response.content)
+    
+    comment.emotion_label = label
+    comment.save()
     return label
 
   def sentiment_model(self, data):
+    comment, created = Comment.objects.get_or_create(
+      text=data, defaults={"sentiment_label": "ERROR"}
+    )
+
+    if comment.sentiment_label and comment.sentiment_label != Comment.SentimentLabel.ERROR:
+      print("Procesado por DB")
+      return comment.sentiment_label
+
+    print("Procesado por API")
     response = self.httpClient.request("POST", SENTIMENT_ENDPOINT, json=data)
     if response.status_code == 200:
       label = response.json()[0][0]["label"]
     else:
-      label = "error"
+      label = "ERROR"
       print(response.content)
+    
+    comment.sentiment_label = label
+    comment.save()
     return label
 
 process_data = ProcessData()
